2a94b05579ce526994553282c45f563d788267cb
rinabati <arinab@gmx.de>
2020-08-20 20:16:09 +0200

    http to https, else mixed content warning

--- assets/plugins/eve/eve.js
+++ assets/plugins/eve/eve.js
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// http://www.apache.org/licenses/LICENSE-2.0
+// https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,7 +14,7 @@
 // ┌────────────────────────────────────────────────────────────┐ \\
 // │ Eve 0.5.4 - JavaScript Events Library                      │ \\
 // ├────────────────────────────────────────────────────────────┤ \\
-// │ Author Dmitry Baranovskiy (http://dmitry.baranovskiy.com/) │ \\
+// │ Author Dmitry Baranovskiy (https://dmitry.baranovskiy.com/) │ \\
 // └────────────────────────────────────────────────────────────┘ \\
 
 (function (glob) {
